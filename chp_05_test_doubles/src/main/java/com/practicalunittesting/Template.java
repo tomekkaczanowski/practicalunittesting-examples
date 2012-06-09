7b710ab09ac0bd8ed127d6ef2642dc44689970c9
Tomek Kaczanowski <tomekkaczanowski@o2.pl>
2012-06-09 21:32:31 +0200

    chapter 5

---
+++ chp_05_test_doubles/src/main/java/com/practicalunittesting/Template.java
@@ -0,0 +1,10 @@
+package com.practicalunittesting.blah;
+
+/**
+ * Sample code for "Developers test: From beginner to master" book.
+ * Tomasz Kaczanowski
+ * http://kaczanowscy.pl/tomek
+ * 3/20/12, 7:44 AM
+ */
+public interface Template {
+}
